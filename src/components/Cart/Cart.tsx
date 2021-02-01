f4f883c5456ccc12a088ad5378840126500215f5
Nur Mohammad Rashed <rashed_huawei@yahoo.com>
2021-02-01 15:48:16 +0100

    first commit

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,101 @@
+import React,{ChangeEvent, useState} from 'react';
+import { Card, Grid} from 'semantic-ui-react';
+import {ICartItem} from '../../App';
+import Axios from 'axios';
+
+interface IMovie{
+  id:number,
+  name:string,
+  description:number,
+  imageUrl:string,
+  price: number
+}
+
+interface ICartProps{
+  message: ICartItem[];
+  removeItem(movie:IMovie):void;
+  clearItem():void;
+} 
+
+
+
+export default function Cart(props:ICartProps) {
+
+  console.log(props);
+  const [customerEmail, setCustomerEmail] = useState('');
+
+  //const tempMessage = props.message;
+  const clearMovie = () =>{
+    props.clearItem();
+  }
+
+  function updateCustomerEmail(e:ChangeEvent<HTMLInputElement>){
+    setCustomerEmail(e.target.value);
+  }
+
+  let totalPrice = props.message.reduce((a, c)=> a + ((c.amount) * c.product.price) , 0)
+
+  let orderMovies = props.message.map((movie: ICartItem) => {
+    return(
+      {      
+      productId: movie.product.id,      
+      amount: movie.amount
+      }
+    ); 
+  });
+
+  async function checkOut() {
+ 
+    let params = {
+      id: 0,
+      /* companyId: 5490,
+      created: new Date(),
+      createdBy: customerEmail, */
+      PaymentType: "mastercard",
+      totalPrice: props.message.reduce((a, c)=> a + ((c.amount) * c.product.price) , 0),
+     /*  status: 0, */
+      OrderDetails: orderMovies,
+      customer: {EmailAddress: customerEmail}
+    }
+    let res = await Axios.post('https://localhost:5001/order',
+    params)
+    console.log(res.data)
+    clearMovie();
+  };
+
+  let movieHtml = props.message.map((movie:ICartItem)=>{
+    return(
+      <Grid.Column key= {movie.product.id}>
+          <Card className="cardHeight">
+              <Card.Content>
+                <Card.Header>{movie.product.name}</Card.Header>                  
+                <Card.Description>
+                      <img className="image" src={movie.product.imageUrl} alt="Movie"/>
+                      <p>Price: {movie.product.price} x {movie.amount}</p>
+                      {/* <button type='button' onClick={()=>clickHandel(movie)}>Add to Cart</button> */}
+                </Card.Description>
+              </Card.Content>
+          </Card>
+          <div>Number of product: {movie.amount}</div>
+          <button type='button' onClick={()=>props.removeItem(movie.product)}>Delete</button>
+          
+      </Grid.Column>
+      
+  )})
+
+ 
+  return (
+    <>
+        
+        {/* <p>{JSON.stringify(props.message)}</p> */}
+        <Grid columns={3}>
+            {movieHtml}            
+        </Grid>
+        <p>Total Price: {totalPrice} </p>
+        <input type='text' id='email' onChange={updateCustomerEmail}></input>
+        <button type='button' onClick={()=>checkOut()}>Proceed</button>
+    </>
+
+    
+  );
+}
